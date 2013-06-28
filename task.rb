12785b0a1493797e862d0b9e4a39d5bdc0997518
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2013-06-28 14:50:20 +0200

    refactor : replacing this_amount temp var with query in customer.statement

--- task.rb
+++ task.rb
@@ -43,7 +43,6 @@ class Rental
 
 end
 
-
 class Customer
   attr_reader :name
 
@@ -60,7 +59,6 @@ class Customer
     total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
-      this_amount = amount_for element
 
       # add frequent renter points
       frequent_renter_points += 1
@@ -69,17 +67,13 @@ class Customer
         frequent_renter_points += 1
       end
       # show figures for this rental
-      result += "\t" + element.movie.title.to_s + "\t" + this_amount.to_s + "\n"
-      total_amount += this_amount
+      result += "\t" + element.movie.title.to_s + "\t" + element.get_charge + "\n"
+      total_amount += element.get_charge
     end
     #add footer lines
     result += "Amount owed is #{total_amount}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
     result
-  end
-
-  def amount_for (rental)
-    return rental.get_charge
   end
 
 end
