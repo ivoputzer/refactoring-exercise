a6f7fd56fec1131765ed5ae883d1af3b373b47fc
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2013-07-02 17:51:56 +0200

    minor fixes variable names, method names, pagination and readability

--- task.rb
+++ task.rb
@@ -1,18 +1,66 @@
-class Movie
+class Price
   
+  def get_points(days_rented)
+    1
+  end
+
+end
+
+class ChildrensPrice < Price
+  
+  def get_price_code
+    Movie::CHILDRENS
+  end
+
+  def get_charge (days_rented)
+    (days_rented > 3) ? 1.5 + ((days_rented - 3) * 1.5) : 1.5
+  end
+
+end
+
+class NewReleasePrice < Price
+  
+  def get_price_code
+    Movie::NEW_RELEASE
+  end
+
+  def get_charge (days_rented)
+    days_rented * 3
+  end
+
+  def get_points(days_rented)
+    (days_rented > 2) ? 2 : super.get_frequent_renter_points(days_rented);
+  end
+
+end
+
+class RegularPrice < Price
+  
+  def get_price_code
+    Movie::REGULAR
+  end
+
+  def get_charge (days_rented)
+    (days_rented > 2) ? 2 + ((days_rented - 2) * 1.5) : 2
+  end
+
+end
+
+class Movie
+
   REGULAR = 0
+  
   NEW_RELEASE = 1
+  
   CHILDRENS = 2
 
-  attr_reader :title
-  
-  attr_reader :price_code
+  attr_reader :title, :price_code
 
   def initialize(title, price_code)
     @title, @price_code = title, set_price_code(price_code)
   end
 
-  def set_price_code (price_code)
+  def set_price_code (price_code) # overloading price_code setter
     case price_code
       when REGULAR
         @price = RegularPrice.new
@@ -23,68 +71,18 @@ class Movie
     end
   end
 
-  def get_price_code
-    return @price_code
-  end
-
   def get_charge (days_rented)
-    return @price.get_charge(days_rented)
+    @price.get_charge(days_rented)
   end
 
-  def get_frequent_renter_points (days_rented)
-    return @price.get_frequent_renter_points(days_rented)
-  end
-end
-
-class Price
-  def get_frequent_renter_points(days_rented)
-    return 1
-  end
-end
-
-class ChildrensPrice < Price
-  def get_price_code
-    return Movie::CHILDRENS
+  def get_points (days_rented)
+    @price.get_points(days_rented)
   end
 
-  def get_charge (days_rented)
-    result = 0
-    result += 1.5
-    result += (days_rented - 3) * 1.5 if days_rented > 3
-    return result
-  end
-end
-
-class NewReleasePrice < Price
-  def get_price_code
-    return Movie::NEW_RELEASE
-  end
-
-  def get_frequent_renter_points(days_rented)
-    return ( days_rented > 2 ) ? 2 : 1;
-  end
-
-  def get_charge (days_rented)
-    result = 0
-    result += days_rented * 3
-    return result
-  end
-end
-
-class RegularPrice < Price
-  def get_price_code
-    return Movie::REGULAR
-  end
-
-  def get_charge (days_rented)
-    result = 0
-    result += 2
-    result += (days_rented - 2) * 1.5 if days_rented > 2
-    return result
-  end
 end
 
 class Rental
+
   attr_reader :movie, :days_rented
 
   def initialize(movie, days_rented)
@@ -92,63 +90,49 @@ class Rental
   end
 
   def get_charge
-    return @movie.get_charge(@days_rented)
+    @movie.get_charge(@days_rented)
   end
 
-  def get_frequent_renter_points
-    return @movie.get_frequent_renter_points(@days_rented)
+  def get_points
+    @movie.get_points(@days_rented)
   end
 
 end
 
 class Customer
+  
   attr_reader :name
 
   def initialize(name)
-    @name = name
-    @rentals = []
+    @name, @rentals = name, []
   end
 
-  def add_rental(arg)
-    @rentals << arg
-  end
-
-  def statement
-    result = "Rental Record for #{@name}\n"
-    @rentals.each do |element|
-      # show figures for this rental
-      result += "\t" + element.movie.title.to_s + "\t" + element.get_charge.to_s + "\n"
-    end
-    #add footer lines
-    result += "Amount owed is #{self.get_total_charge}\n"
-    result += "You earned #{self.get_total_frequent_renter_points} frequent renter points"
-    result
-  end
-
-  def html_statement
-    result = "<h1>Rentals for <em>#{@name}</em></h1>\n<p>";
-    @rentals.each do |element|
-      result += "#{element.movie.title.to_s} : #{element.get_charge.to_s} <br>\n";
-    end 
-    result += "</p>\n<p>Amount owed is <em>#{self.get_total_charge}</em></p>\n"
-    result += "<p>You earned <em>#{self.get_total_frequent_renter_points}</em> frequent renter points</p>"
-    return result
+  def add_rental(rental)
+    @rentals << rental
   end
 
   def get_total_charge
     result = 0
-    @rentals.each do |each| 
-      result += each.get_charge
-    end
-    return result
+    @rentals.each { |rental| result += rental.get_charge }
+    result
   end
 
-  def get_total_frequent_renter_points
+  def get_total_points
     result = 0
-    @rentals.each do |each|
-      result += each.get_frequent_renter_points
-    end
-    return result
+    @rentals.each { |rental| result += rental.get_points }
+    result
+  end
+
+  def statement
+    text = "Rental Record for #{@name}\n"
+    @rentals.each { |rental| text += "\t#{rental.movie.title}\t#{rental.get_charge}\n" }
+    text += "Amount owed is #{get_total_charge}\nYou earned #{get_total_points} frequent renter points"
+  end
+
+  def html_statement
+    html = "<h1>Rentals for <em>#{@name}</em></h1>\n<p>";
+    @rentals.each { |rental| html += "#{rental.movie.title} : #{rental.get_charge} <br>\n" }
+    html += "</p>\n<p>Amount owed is <em>#{get_total_charge}</em></p>\n<p>You earned <em>#{get_total_points}</em> frequent renter points</p>"
   end
 
 end
