0288993adcbbf1ec08c8edc9565b24457908a714
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2013-07-02 15:45:57 +0200

    adding test for customer.html_statement with multiple movies

--- unit.rb
+++ unit.rb
@@ -48,26 +48,24 @@ class UnitTest < Test::Unit::TestCase
 
   end
 
-# # # # TODO
-#
-#  def test_multiple_movies_html
-#
-#      customer = Customer.new :Ivo
-#
-#      rental1 = Rental.new (Movie.new :Armageddon, Movie::REGULAR), 5
-#
-#      rental2 = Rental.new (Movie.new :Shrek, Movie::CHILDRENS), 6
-#
-#      rental3 = Rental.new (Movie.new :Armageddon, Movie::NEW_RELEASE), 3
-#
-#      customer.add_rental rental1
-#
-#      customer.add_rental rental2
-#
-#      customer.add_rental rental3
-#
-#      assert_equal("Rental Record for Ivo\n\tArmageddon\t6.5\n\tShrek\t6.0\n\tArmageddon\t9\nAmount owed is 21.5\nYou earned 4 frequent renter points", customer.statement)
-#
-#  end
+  def test_multiple_movies_html
+
+      customer = Customer.new :Ivo
+
+      rental1 = Rental.new (Movie.new :Armageddon, Movie::REGULAR), 5
+
+      rental2 = Rental.new (Movie.new :Shrek, Movie::CHILDRENS), 6
+
+      rental3 = Rental.new (Movie.new :Armageddon, Movie::NEW_RELEASE), 3
+
+      customer.add_rental rental1
+
+      customer.add_rental rental2
+
+      customer.add_rental rental3
+
+      assert_equal("<h1>Rentals for <em>Ivo</em></h1>\n<p>Armageddon : 6.5 <br>\nShrek : 6.0 <br>\nArmageddon : 9 <br>\n</p>\n<p>Amount owed is <em>21.5</em></p>\n<p>You earned <em>4</em> frequent renter points</p>", customer.html_statement)
+
+  end
 
 end
